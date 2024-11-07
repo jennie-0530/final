4dcd80bf57e7c92d46cde91e7eebe840c11cd886
KooTaehyeon <vcro12123@naver.com>
2024-11-07 12:05:15 +0900

    |   :tada:   |     start      |        프로젝트 시작        |

---
+++ final_be/app.ts
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// http
+const { createServer } = require('node:http');
+const port: number = 3333;
+
+// 서버
+const app = express();
+const server = createServer(app);
+
+// 미들웨어 설정
+app.use(morgan('dev'));
+app.use(
+  cors({
+    origin: 'http://localhost:3000', // 클라이언트 주소
+    credentials: true, // 쿠키를 포함하여 요청을 보내도록 설정
+  })
+);
+
+// 요청 본문 크기 제한을 10MB로 설정 (필요에 따라 더 크게 조정 가능)
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
+server.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
